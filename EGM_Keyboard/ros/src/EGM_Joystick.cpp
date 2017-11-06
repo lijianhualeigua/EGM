a8117a1cbe722857dc4481ad1471830169a567e5
Shuyang Chen <shuyangchen01@gmail.com>
2017-11-06 14:16:01 -0500

    code cleanup

--- EGM_Keyboard/ros/src/EGM_Joystick.cpp
+++ EGM_Keyboard/ros/src/EGM_Joystick.cpp
@@ -15,67 +15,68 @@ bool joint_cmd = true;
 pos pos;
 
 void joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {   
-    for (int i = 0; i < 6; i++) {
-        pos.joints[i] = feedback_joints[i] + ((double)joy->buttons[i]) * 10.0;
-    }
+	for (int i = 0; i < 6; i++) {
+		pos.joints[i] = feedback_joints[i] + ((double)joy->buttons[i]) * 10.0;
+	}
 }
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "joy_to_EGM");
-    ros::NodeHandle nh;
-    ros::Subscriber sub;
-    int sockfd;
-    socklen_t len;
-    int n;
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        printf("cannot create socket");
-        return 0;
-    }
-    
-    struct sockaddr_in serverAddr, clientAddr;
-    memset(&serverAddr, sizeof(serverAddr), 0);
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serverAddr.sin_port = htons(portNumber);
+	ros::init(argc, argv, "joy_to_EGM");
+	ros::NodeHandle nh;
+	ros::Subscriber sub;
+	int sockfd;
+	socklen_t len;
+	int n;
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		printf("cannot create socket");
+		return 0;
+	}
 
-    int result = bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)); 
-    
-    if (!result) {
-    sub = nh.subscribe<sensor_msgs::Joy>("/joy", 1000, &joyCallback); 
-    while (ros::ok()) {
-        // receive and display message from robot
-        len = sizeof(clientAddr);
-        n = recvfrom(sockfd, protoMessage, 1400, 0, (struct sockaddr *)&clientAddr, &len);
-        if (n < 0) {
-            printf("Error receive message\n");
-            continue;
-        }
-        // parse inbound message
-        EgmRobot *pRobotMessage = new EgmRobot();
-        pRobotMessage->ParseFromArray(protoMessage, n);
-        DisplayRobotMessage(pRobotMessage);
-        //delete pRobotMessage;
+	struct sockaddr_in serverAddr, clientAddr;
+	memset(&serverAddr, sizeof(serverAddr), 0);
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddr.sin_port = htons(portNumber);
 
-        // current joint position
-        for (int i = 0; i < 6; i++) {
-            feedback_joints[i] = (pRobotMessage->feedback().joints().joints(i)) * rad_deg;
-        } 
-        
-        // create and send a sensor message
-        EgmSensor *pSensorMessage = new EgmSensor();
-        CreateSensorMessage(pSensorMessage, pos, joint_cmd);
-        pSensorMessage->SerializeToString(&messageBuffer);
+	int result = bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)); 
 
-        // send a message to the robot
-        n = sendto(sockfd, messageBuffer.c_str(), messageBuffer.length(), 0, (struct sockaddr *)&clientAddr, sizeof(clientAddr));
-        if (n < 0) {
-            printf("Error send message\n");
-            }
-        //delete pSensorMessage;
-        ros::spinOnce();
-    } 
+	if (!result) {
+		sub = nh.subscribe<sensor_msgs::Joy>("/joy", 1000, &joyCallback); 
+		while (ros::ok()) {
+			// receive and display message from robot
+			len = sizeof(clientAddr);
+			n = recvfrom(sockfd, protoMessage, 1400, 0, (struct sockaddr *)&clientAddr, &len);
+			if (n < 0) {
+				printf("Error receive message\n");
+				continue;
+			}
+			// parse inbound message
+			EgmRobot *pRobotMessage = new EgmRobot();
+			pRobotMessage->ParseFromArray(protoMessage, n);
+			DisplayRobotMessage(pRobotMessage);
+			//delete pRobotMessage;
+
+			// current joint position
+			for (int i = 0; i < 6; i++) {
+				feedback_joints[i] = (pRobotMessage->feedback().joints().joints(i)) * rad_deg;
+			} 
+
+			// create and send a sensor message
+			EgmSensor *pSensorMessage = new EgmSensor();
+			CreateSensorMessage(pSensorMessage, pos, joint_cmd);
+			pSensorMessage->SerializeToString(&messageBuffer);
+
+			// send a message to the robot
+			n = sendto(sockfd, messageBuffer.c_str(), messageBuffer.length(), 0, (struct sockaddr *)&clientAddr, sizeof(clientAddr));
+			if (n < 0) {
+				printf("Error send message\n");
+			}
+			//delete pSensorMessage;
+			ros::spinOnce();
+		} 
+	}
+	else 
+		return -1;
+	//close(sockfd);
 }
-    else 
-        return -1;
-    //close(sockfd);
-}
+
